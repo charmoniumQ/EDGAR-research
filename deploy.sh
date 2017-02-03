8a31a09e9caaa8b1e8cad4f8936af57b366984cb
charmoniumQ <sam@samgrayson.me>
2017-02-03 17:28:19 -0600

    write risk factor searching

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+topic="write risk factor searching"
+
+user="sam"
 host="104.196.27.128"
-droppath="~/p/"
+dest="~/EDGAR-research"
 identity="~/.ssh/google_compute_engine"
 
+git commit -m "$topic"
 
+git push --all origin
 
-# ssh $host "cd $droppath/mining ; "
+ssh -i $identity $user@$host "cd $dest ; git pull ; cd mining ; ./extract_risk.py"
