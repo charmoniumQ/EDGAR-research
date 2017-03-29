c09071a3761a35c9764f4ae25818834b53fc4a80
Shayan Monadjemi <shayan1994@gmail.com>
2017-03-29 15:55:38 -0500

    fixed extra new line issue on windows

--- analysis/manual_classification.py
+++ analysis/manual_classification.py
@@ -5,7 +5,7 @@ paragraphFilePath = "data/paragraphs.csv"
 responseFilePath = "data/responses.csv"
 
 responseDictionary = {}
-#create the response file if it doesn't exist
+# create the response file if it doesn't exist
 if os.path.isfile(responseFilePath):
     with open(responseFilePath, "r") as responseFile:
         responseReader = csv.DictReader(responseFile)
@@ -17,9 +17,7 @@ else:
         responseFile.write("number,response\n")
     responseFile.close()
 
-
-
-with open(paragraphFilePath) as paragraphFile, open(responseFilePath, "a") as responseFile:
+with open(paragraphFilePath) as paragraphFile, open(responseFilePath, "a", newline="") as responseFile:
     paragraphReader = csv.DictReader(paragraphFile)
     responseWriter = csv.writer(responseFile)
     for paragraph in paragraphReader:
