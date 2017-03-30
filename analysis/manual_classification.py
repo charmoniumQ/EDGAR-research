19c15b11cb889c11fdf3ac3d9426c7904cd9394f
Shayan Monadjemi <shayan1994@gmail.com>
2017-03-30 10:51:55 -0500

    added comments and changed some output format

--- analysis/manual_classification.py
+++ analysis/manual_classification.py
@@ -1,41 +1,45 @@
 import csv
 import os.path
 
-responseDictionary = {}
-
 # Login with username
-username = input("Please enter username: ")
+username = input("-Please enter username: ")
 while username == "":
     username = input("Error: username can't be blank\nPlease enter username: ")
 
+
+# File paths used
 paragraphFilePath = "data/paragraphs.csv"
 responseFilePath = "data/responses_" + username + ".csv"
 
+# Dictionary to hold previously entered responses
+responseDictionary = {}
 
-# create the response file if it doesn't exist
+# create the response file if it doesn't exist; if it does exist, load it into responseDictionary
 if os.path.isfile(responseFilePath):
     with open(responseFilePath, "r") as responseFile:
         responseReader = csv.DictReader(responseFile)
         for row in responseReader:
             responseDictionary[row['number']] = row['response']
-        print(responseDictionary)
 else:
     with(open(responseFilePath, "w+")) as responseFile:
         responseFile.write("number,response\n")
     responseFile.close()
 
+print("+Found", len(responseDictionary), " previously recorded responses by", username, "\n")
+
+# Go through the remaining paragraphs and record the user's response in the corresponding response file.
 with open(paragraphFilePath) as paragraphFile, open(responseFilePath, "a", newline="") as responseFile:
     paragraphReader = csv.DictReader(paragraphFile)
     responseWriter = csv.writer(responseFile)
     for paragraph in paragraphReader:
         if paragraph['number'] not in responseDictionary.keys():
-            print(paragraph['text'])
-            userResponse = input("\n******\nIs this related to topic? (Y/N), or quit   ")
+            print("#%s: %s" % (paragraph['number'], paragraph['text']))
+            userResponse = input("-Is this related to topic? (Y/N), or quit   ")
             while userResponse not in ('y', 'n', 'Y', 'N', 'quit'):
-                userResponse = input("Bad input... Is this related to topic? (Y/N)   ")
+                userResponse = input("-Bad input... Is this related to topic? (Y/N)   ")
             if userResponse == 'quit':
                 break
-            print("your response is ", userResponse.upper(), "\n******\n")
+            print("+Your response, %s, is recorded\n" % userResponse.upper())
             responseDictionary[paragraph['number']] = userResponse
             field = [paragraph['number'], userResponse.upper()]
             responseWriter.writerow(field)
