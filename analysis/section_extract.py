d64e17edefb2c7eaa6e6fc05d2eec7b9e7d078d6
Shayan Monadjemi <shayan1994@gmail.com>
2017-05-06 14:51:07 -0500

    extract sections - made the code better

--- analysis/section_extract.py
+++ analysis/section_extract.py
@@ -4,18 +4,21 @@ from mining.retrieve_10k import get_risk_factors
 
 year = 2016
 qtr = 3
-type = '10-k'
+form_type = '10-k'
 
 
 
 def get_sections(year, qtr):
     for record in get_index(year, qtr, enable_cache=True, verbose=False, debug=True):
         rf = get_risk_factors(record['Filename'], enable_cache=True, verbose=False, debug=False, throw=False)
-        path = 'results/risk-factors/' + type + '-' + str(year) + "-" + str(qtr) + "-" + str(record['CIK']) + ".txt"
-        if str(rf) != 'None':
-            with open(path, 'w+') as f:
-                f.write(str(rf))
+        path = 'results/risk-factors/' + form_type + '-' + str(year) + "-" + str(qtr) + "-" + str(record['CIK']) + ".txt"
+        if isinstance(rf, str) and len(rf) > 1000:
+            with open(path, 'w+', encoding='utf-8') as f:
+                f.write(rf)
             f.close()
+
+
+
 
 
 
