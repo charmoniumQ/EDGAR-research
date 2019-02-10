6ef0019bb8634a286a66f79b934f1be02e89c336
Samuel Grayson <sam@samgrayson.me>
2019-02-09 18:22:14 -0600

    changed deploy to have fewer subprocess calls

--- edgar_deploy/gke_cluster.py
+++ edgar_deploy/gke_cluster.py
@@ -1,12 +1,14 @@
 from .config import config
 from .cluster import Cluster
-import google
-from google.cloud import container_v1
-import kubernetes
 import subprocess
 import base64
 import json
 import time
+import google
+# https://googleapis.github.io/google-cloud-python/latest/container/gapic/v1/api.html
+from google.cloud import container_v1
+# https://github.com/kubernetes-client/python
+import kubernetes
 
 
 class GKECluster(Cluster):
@@ -46,35 +48,36 @@ class GKECluster(Cluster):
                 ),
             ),
         )
-        #self.gke_cluster = self.cluster_manager.create_cluster(None, None, self.gke_cluster, parent=self.parent_path)
-        self.gke_cluster = self.cluster_manager.get_cluster(None, None, None, name=self.name_path)
+        self.cluster_manager.create_cluster(None, None, self.gke_cluster, parent=self.parent_path)
+        #self.gke_cluster = self.cluster_manager.get_cluster(None, None, None, name=self.name_path)
+        #print(self.gke_cluster)
 
         # tried: https://banzaicloud.com/blog/pipeline-gke-rbac/
         # tried: https://stackoverflow.com/a/48377444/1078199
         # using: https://stackoverflow.com/questions/54410410/authenticating-to-gke-master-in-python/54534575#54534575
-        subprocess.run(['gcloud', '--quiet', 'container', 'clusters',
-                        'get-credentials', self.gke_cluster.name,
-                        '--region', self.gke_cluster.location],
-                       capture_output=True)
+        subprocess.run([
+            'gcloud', '--quiet', 'container', 'clusters',
+            'get-credentials', self.gke_cluster.name,
+            '--region', self.gke_cluster.location
+        ], capture_output=True)
 
-        subprocess.run(['kubectl', 'create', '-f', '-'], input=json.dumps({
+        objects = [
+            {
                 'apiVersion': 'v1',
                 'kind': 'Namespace',
                 'metadata': {
                     'name': self.namespace,
                 },
-            }).encode())
-
-        subprocess.run(['kubectl', 'create', '-f', '-'], input=json.dumps({
+            },
+            {
                 'apiVersion': 'v1',
                 'kind': 'ServiceAccount',
                 'metadata': {
                     'name': self.username,
                     'namespace': self.namespace,
                 },
-            }).encode())
-
-        subprocess.run(['kubectl', 'create', '-f', '-'], input=json.dumps({
+            },
+            {
                 'apiVersion': 'rbac.authorization.k8s.io/v1',
                 'kind': 'RoleBinding',
                 'metadata': {
@@ -93,7 +96,13 @@ class GKECluster(Cluster):
                     'kind': 'ClusterRole',
                     'name': 'cluster-admin',
                 },
-            }).encode())
+            },
+        ]
+
+        for i, object_ in enumerate(objects):
+            with open(config.scratch_dir / f'{i}.json', 'w') as f:
+                json.dump(object_, f)
+        subprocess.run(['kubectl', 'apply'] + flatten1([('-f', f'{i}.json') for i in range(objects)]))
 
         self._configure_access()
 
@@ -111,30 +120,24 @@ class GKECluster(Cluster):
         self.gke_cluster = None
 
     def _configure_access(self):
-        secret_name = subprocess.run(['kubectl',  '--namespace', self.namespace,
-                                      'get', f'serviceaccounts/{self.username}',
-                                      '-o', 'jsonpath={.secrets[0].name}'],
-                                     capture_output=True, encoding='ascii').stdout
+        secret_name = subprocess.run([
+            'kubectl',  '--namespace', self.namespace,
+            'get', f'serviceaccounts/{self.username}',
+            '-o', 'jsonpath={.secrets[0].name}'
+        ], capture_output=True, encoding='ascii').stdout
         if not secret_name:
             raise RuntimeError(f'no secret corresponding to serviceaccounts/{self.username}')
 
-        client_cert = subprocess.run(['kubectl',  '--namespace', self.namespace,
-                                      'get', f'secret/{secret_name}',
-                                      '-o', 'jsonpath={.data.ca\.crt}'],
-                                     capture_output=True).stdout
-        if not client_cert:
-            raise RuntimeError(f'no client cert in secret/{secret_name}')
-
-        client_token = subprocess.run(['kubectl',  '--namespace', self.namespace,
-                                       'get', f'secret/{secret_name}',
-                                       '-o', 'jsonpath={.data.token}'],
-                                      capture_output=True).stdout
-        if not client_cert:
-            raise RuntimeError(f'no client token in secret/{secret_name}')
+        secret = json.loads(subprocess.run([
+            'kubectl',  '--namespace', self.namespace,
+            'get', f'secret/{secret_name}', '-o', 'json'
+        ], capture_output=True).stdout)
+        client_cert =  base64.decodebytes(secret['data']['ca.crt'].encode())
+        client_token = base64.decodebytes(secret['data']['token'].encode()).decode()
 
         self.kube_config = kubernetes.client.Configuration()
         self.kube_config.verify_ssl = True
-        self.kube_config.api_key['authorization'] = base64.decodebytes(client_token).decode()
+        self.kube_config.api_key['authorization'] = client_token
         self.kube_config.api_key_prefix['authorization'] = 'Bearer'
 
         for logger in self.kube_config.logger.values():
@@ -149,10 +152,14 @@ class GKECluster(Cluster):
 
         # self.kube_config.cert_file = dire / 'cert_file'
         # with self.kube_config.cert_file.open('wb') as f:
-        #     f.write(base64.decodebytes(client_cert))
+        #     f.write(client_cert)
 
         self.kube_api = kubernetes.client.ApiClient(configuration=self.kube_config)
         self.kube_v1 = kubernetes.client.CoreV1Api(self.kube_api)
+
+
+def flatten1(list_):
+    return [elem for list2 in list1 for elem in list2]
 
 
 if __name__ == '__main__':
