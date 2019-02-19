e2937c009b1feafa6b0c0e11d897b198a9faf6b2
Samuel Grayson <sam@samgrayson.me>
2019-02-18 23:28:01 -0600

    added parallel script

--- edgar_deploy/cluster.py
+++ edgar_deploy/cluster.py
@@ -7,25 +7,36 @@ class Cluster(object):
         self.nodecount = nodecount
         self.load = load
         self.save = save
+        self.opened = False
 
     def __enter__(self):
-        if self.load:
-            logging.info('Loading cluster')
-            if self._load():
-                pass
-            else:
-                logging.info('No cluster to load; creating cluster')
-                self._create()
-        else:
-            logging.info('Creating cluster')
-            self._create()
+        self.open()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if not self.save:
-            logging.info('Deleting cluster')
-            self._delete()
-        else:
-            logging.info('Not deleting cluster')
+        self.close()
+
+    def open(self):
+        if not self.opened:
+            self.opened = True
+            if self.load:
+                logging.info('Loading cluster')
+                if self._load():
+                    pass
+                else:
+                    logging.info('No cluster to load; creating cluster')
+                    self._create()
+            else:
+                logging.info('Creating cluster')
+                self._create()
+
+    def close(self):
+        if self.opened:
+            self.opened = False
+            if not self.save:
+                logging.info('Deleting cluster')
+                self._delete()
+            else:
+                logging.info('Not deleting cluster')
 
     def _load(self):
         # return True iff cluster could be loaded
---
+++ edgar_deploy/main.py
@@ -0,0 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
+from .gke_cluster import GKECluster
+from .kubernetes_deploy import prepare_images, deploy_kubernetes
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    cluster = GKECluster('test-cluster-2', load=True, save=True)
+
+    images_fut = executor.submit(prepare_images)
+    cluster_fut = executor.submit(cluster.open)
+
+    images = images_fut.result()
+    cluster_fut.result()
+
+    with cluster:
+        with deploy_kubernetes(cluster):
+            print('done ish')
+            input()
